7e2e289f576c4d4d3bdb7b9c55c06af266fa1517
Jorge <jorge.membrillo.g@gmail.com>
2026-05-14 00:25:46 -0500

    CORS

--- src/main/java/com/kaislo/rooftop/controllers/ReservaParrillaController.java
+++ src/main/java/com/kaislo/rooftop/controllers/ReservaParrillaController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservas")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ReservaParrillaController {
 
     private final ReservaParrillaService reservaService;
